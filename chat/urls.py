a3f5b1dfacf302d4da6aa6e5d45b6ff652f9d9ad
Ezio <assassinste@gmail.com>
2020-01-27 16:59:05 +0800

    Add the room view (no actual functionality was implemented yet)

--- chat/urls.py
+++ chat/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [path(route="", view=views.index, name="index")]
-
+urlpatterns = [
+    path(route="", view=views.index, name="index"),
+    path(route="<str:room_name>/", view=views.room, name="room"),
+]
--- chat/views.py
+++ chat/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request=request, template_name="chat/index.html", context={})
+
+
+def room(request, room_name):
+    return render(
+        request=request,
+        template_name="chat/room.html",
+        context={"room_name": room_name},
+    )
